890be295309881450c219bfd7e1b39db32079fe7
repalleramr <repalleramr@gmail.com>
2026-04-13 12:44:43 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,8 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('ipctv2').then(c=>c.addAll(['./'])))
+const CACHE = "ipct-final-cache-v2";
+const ASSETS = ["./","./index.html","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-192-maskable.png","./icons/icon-512-maskable.png"];
+self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))); self.clients.claim(); });
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => { const copy = resp.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{}); return resp; }).catch(() => caches.match("./index.html"))));
 });
